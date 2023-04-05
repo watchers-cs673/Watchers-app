fe824337caf55b3a721e345cff73d7c52a4253b1
Sophie <sophiemcq@live.com>
2023-04-05 15:51:43 -0400

    fix search component and profile list pages

--- watchers-app/src/app/profile/profile.component.ts
+++ watchers-app/src/app/profile/profile.component.ts
@@ -15,11 +15,27 @@ interface UserData {
 export class ProfileComponent {
     public userName = 'Test User';
     public userData: UserData = {posts: 2, followers: 3, following: 2};
+    public showList = false;
+    public listTitle = '';
+
     constructor(public auth: AuthService) {
       auth.user$.subscribe((data) => {
         if(data?.email) {
           this.userName = data.email;
         }
       })
+    }
+
+    public showFollowers() {
+      this.showList=true;
+      this.listTitle= "Followers";
+    }
+    public showPosts() {
+      this.showList=true;
+      this.listTitle= "Posts";
+    }
+    public showFollowing() {
+      this.showList=true;
+      this.listTitle= "Following";
     }
 }
--- watchers-app/src/app/search/search.component.ts
+++ watchers-app/src/app/search/search.component.ts
@@ -53,14 +53,4 @@ export class SearchComponent implements OnInit{
     }
   }
 
-  // catogories = [
-  //   {src: 'assets/pic1_4.png', title: 'movie 1', link: '/discussion'},
-  //   {src: 'assets/pic1_5.png', title: 'movie 2', link: '/discussion'},
-  //   {src: 'assets/pic1_8.png', title: 'movie 3', link: '/discussion'},
-  //   {src: 'assets/pic2_8.png', title: 'movie 4', link: '/discussion'},
-  //   {src: 'assets/pic2_7.png', title: 'movie 5', link: '/discussion'},
-  //   {src: 'assets/pic1_3.png', title: 'movie 6', link: '/discussion'},
-  // ];
-
-  
 }
