b1e381580b45ca24c11f55dccb5bc526547d5262
Sophie <sophiemcq@live.com>
2023-04-05 20:06:58 -0400

    create Movie service

--- watchers-app/src/app/search/search.component.ts
+++ watchers-app/src/app/search/search.component.ts
@@ -2,53 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MovieService } from '../services/movie-service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit{
-  searchTerm: string = '';
-  catogories: any[];
-  filteredCatogories: any[];
-  options: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-  filteredOptions: any[] = [];
+export class SearchComponent {
+  public searchTerm: string = '';
+  public catogories: any[];
+  public movies: any[];
+  public filteredMovies: any[];
 
-  constructor() {
-    this.catogories = [
-      {src: 'assets/pic1_4.png', title: 'movie 1', link: '/discussion'},
-      {src: 'assets/pic1_5.png', title: 'movie 2', link: '/discussion'},
-      {src: 'assets/pic1_8.png', title: 'movie 3', link: '/discussion'},
-      {src: 'assets/pic2_8.png', title: 'movie 4', link: '/discussion'},
-      {src: 'assets/pic2_7.png', title: 'movie 5', link: '/discussion'},
-      {src: 'assets/pic1_3.png', title: 'movie 6', link: '/discussion'},
-      {src: "assets/poster1_1.jpg", title: 'avatar', link: '/discussion'},
-    ];
-    this.filteredCatogories = this.catogories;
+  constructor(private movieService: MovieService) {
+    this.catogories = movieService.allMovies.map(movie => movie.genre);
+    this.movies = movieService.allMovies;
+    this.filteredMovies = this.movies;
   }
 
-  users: string[] = ['User1', 'User2', 'User3'];
-
-  onSearch() {
-    this.filteredCatogories = this.catogories.filter(catogory => 
-      catogory.title.toLowerCase().includes(this.searchTerm.toLowerCase())
-    );
-  }
-
-  ngOnInit() {
-    this.filteredOptions = this.catogories;
-  }
-
-  filterOptions() {
-    if (this.searchTerm.startsWith('@')) { // User search
-    this.filteredOptions = this.users.filter(user =>
-      user.toLowerCase().includes(this.searchTerm.substr(1).toLowerCase())
-    );
-    } else {
-      // Search for categories
-      this.filteredOptions = this.catogories.filter(category =>
-        category.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+  public onSearch() {
+    if (this.searchTerm.startsWith('@')){
+      console.log('TODO: user search')
+    }
+    else {
+      this.filteredMovies = this.movies.filter(movie => 
+        movie.name.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
     }
   }
