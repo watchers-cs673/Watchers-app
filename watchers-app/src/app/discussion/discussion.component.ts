59222da1230547fb7308830711a572b0d822c9d5
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2023-04-17 12:18:19 -0400

    added movie search icons route

--- watchers-app/src/app/discussion/discussion.component.ts
+++ watchers-app/src/app/discussion/discussion.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { StarRatingComponent } from '../star-rating/star-rating.component';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-discussion',
   templateUrl: './discussion.component.html',
   styleUrls: ['./discussion.component.css']
 })
-export class DiscussionComponent {
-  title = "wAtchers";
+export class DiscussionComponent implements OnInit{
+  title = "watchers";
   rating: number = 0;
   // current tab
   selectedTab = 'summary';
@@ -26,4 +28,14 @@ export class DiscussionComponent {
     {topic: 'Discussion Topic 6', content: 'Content 6...', date: 'date', author: 'author'},
     // ...
   ];
+
+  name: string | null = null;
+  imgPath: string | null = null;
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.imgPath = this.route.snapshot.paramMap.get('imgPath');
+    this.name = this.route.snapshot.paramMap.get('name');
+  }
+
 }
