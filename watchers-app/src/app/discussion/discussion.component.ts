629bbb8422445143485f1996ca026ea9a4120408
Shreya Surbhi <shreyasurbhi@Shreyas-MacBook-Pro.local>
2023-04-23 23:38:43 -0400

    addReviews

--- watchers-app/src/app/discussion/discussion.component.ts
+++ watchers-app/src/app/discussion/discussion.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { StarRatingComponent } from '../star-rating/star-rating.component';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../services/movie-service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 interface Movie {
   name: string;
@@ -14,6 +13,15 @@ interface Movie {
   summary?: string;
 }
 
+interface Comment {
+  author: string;
+  date: string;
+  rating: number;
+  content: string;
+  topic: string; 
+}
+
+
 @Component({
   selector: 'app-discussion',
   templateUrl: './discussion.component.html',
@@ -24,18 +32,37 @@ export class DiscussionComponent {
   public rating: number = 0;
   // current tab
   public selectedTab = 'summary';
-  public movie$: Observable<Movie | undefined>;
+  //public movie$: Observable<Movie | undefined>;
+  public movie$: Observable<Movie | undefined> = of(undefined);
+  public comments: Comment[] = [];
+  public comment: string = '';
   name: string = "";
   year: string = "";
   imgPath: string = "";
   description: string = "";
 
-  // switch tab
+  
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) { }
+
+  ngOnInit(): void {
+    let movieName = this.route.snapshot.paramMap.get('name');
+    if (name !== null) {
+      this.movie$ = this.movieService.getMovie(movieName ? movieName : "");
+      // Load comments for this movie from a service and assign them to this.comments
+    }
+  }
+  
+   // switch tab
   selectTab(tabName: string) {
     this.selectedTab = tabName;
   }
 
-  comments = [
+  Comment = [
     {topic: 'Discussion Topic 1', content: 'Content 1...', date: 'date', author: 'author'},
     {topic: 'Discussion Topic 2', content: 'Content 2...', date: 'date', author: 'author'},
     {topic: 'Discussion Topic 3', content: 'Content 3...', date: 'date', author: 'author'},
@@ -44,11 +71,23 @@ export class DiscussionComponent {
     {topic: 'Discussion Topic 6', content: 'Content 6...', date: 'date', author: 'author'},
     // ...
   ];
+  
 
-
-  constructor(private route: ActivatedRoute, private movieService: MovieService) {  
-    let movieName = this.route.snapshot.paramMap.get('name');
-    this.movie$ = movieService.getMovie(movieName ? movieName : "");
+  submitForm() {
+    if (!this.comment) {
+      return;
+    }
+    const newComment: Comment = {
+      author: 'User',
+      date: new Date().toISOString(),
+      rating: this.rating,
+      content: this.comment,
+      topic: 'Reviews',
+    };
+    // Add the new comment to the comments array
+    this.comments.push(newComment);
+    // Reset the form
+    this.rating = 0;
+    this.comment = '';
   }
-
 }
